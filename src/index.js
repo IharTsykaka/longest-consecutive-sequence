a5655456602ad94a0ab70d3707badc8b32904a39
IharTsykala <Sudja09@bk.ru>
2018-09-30 23:27:13 +0300

    2018q3SecondBetterSolution

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 module.exports = function longestConsecutiveLength(array) {
 	array.sort(compareNumeric);
+	
 	let currentSum = 1;
 	let maxSum = 0
+	let step = 0;
 	if (array.length === 0) return 0;
 	if (array.length === 1) return 1;
 	
 	for (let i = 1, len = array.length; i < len; i++) {
-	  if (array[i] === array[i-1]+1)  currentSum++;       
-	  else {
-		if(currentSum > maxSum) maxSum = currentSum;
-		currentSum = 1;
-	  }
+		if (array[i] === array[i-1]) continue;
+		if (array[i] === array[i-1]+1)  currentSum++;		
+		if (array[i] === array[i-1]+1 && i === len-1 && currentSum > maxSum) {
+			return maxSum = currentSum;
+		}   
+	  if (array[i] !== array[i-1]+1) {
+			if(currentSum > maxSum) maxSum = currentSum;
+			currentSum = 1;
+		}
+		
 	}
 	return maxSum;
   }
